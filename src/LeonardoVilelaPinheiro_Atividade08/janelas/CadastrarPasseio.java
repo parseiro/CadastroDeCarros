bb8f509ccb6db82b1295e2bb81ee71d4b4535543
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2021-05-17 12:46:17 -0300

    comitar2

--- src/LeonardoVilelaPinheiro_Atividade08/janelas/CadastrarPasseio.java
+++ src/LeonardoVilelaPinheiro_Atividade08/janelas/CadastrarPasseio.java
@@ -11,10 +11,18 @@ public class CadastrarPasseio {
     private static JButton btNovo = new JButton("Novo");
     private static JButton btSair = new JButton("Sair");
 
+    private static JTextField txtQdePassageiros = new JTextField(10);
+    private static JTextField txtPlaca = new JTextField(10);
+    private static JTextField txtMarca = new JTextField(10);
+    private static JTextField txtModelo = new JTextField(10);
+    private static JTextField txtCor = new JTextField(10);
+    private static JTextField txtQdeRodas = new JTextField(10);
+    private static JTextField txtVelocidade = new JTextField(10);
+    private static JTextField txtQdePistoes = new JTextField(10);
+    private static JTextField txtPotencia = new JTextField(10);
+
     public static void main(String... args) {
-        EventQueue.invokeLater(() -> {
-            coisar();
-        });
+        EventQueue.invokeLater(CadastrarPasseio::coisar);
     }
 
     private static void coisar() {
@@ -31,7 +39,40 @@ public class CadastrarPasseio {
 
         int line = 0;
 
-        tela.add(new JLabel("Qtde passageiros:"), new GBC(0, line))
+        tela.add(new JLabel("Qtde passageiros:"), new GBC(0, line));
+        tela.add(txtQdePassageiros, new GBC(1, line, 3, 1));
+        line++;
+
+        tela.add(new JLabel("Placa:"), new GBC(0, line));
+        tela.add(txtPlaca, new GBC(1, line, 3, 1));
+        line++;
+
+        tela.add(new JLabel("Marca:"), new GBC(0, line));
+        tela.add(txtMarca, new GBC(1, line, 3, 1));
+        line++;
+
+        tela.add(new JLabel("Modelo:"), new GBC(0, line));
+        tela.add(txtModelo, new GBC(1, line, 3, 1));
+        line++;
+
+        tela.add(new JLabel("Cor:"), new GBC(0, line));
+        tela.add(txtCor, new GBC(1, line, 3, 1));
+        line++;
+
+        tela.add(new JLabel("Qtd rodas:"), new GBC(0, line));
+        tela.add(txtQdeRodas, new GBC(1, line, 3, 1));
+        line++;
+
+        tela.add(new JLabel("Veloc max (km/h):"), new GBC(0, line));
+        tela.add(txtVelocidade, new GBC(1, line, 3, 1));
+        line++;
+
+        tela.add(new JLabel("Qtd pistões:"), new GBC(0, line));
+        tela.add(txtQdePistoes, new GBC(1, line, 3, 1));
+        line++;
+
+        tela.add(new JLabel("Potência:"), new GBC(0, line));
+        tela.add(txtPotencia, new GBC(1, line, 3, 1));
         line++;
 
         tela.add(btCadastrar, new GBC(0, line).setAnchor(GridBagConstraints.CENTER));
